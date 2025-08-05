a3d24c6c87b3b309dd1a9d1218f498c5415ff0eb
hlengoc <hlengoc.fpt@hillspire.com>
2025-08-05 02:31:31 +0000

    Create setup-pg.ps1

---
+++ postgres/setup-pg.ps1
@@ -0,0 +1,21 @@
+choco install postgresql --version=16.8.0 -y
+# Set file path
+$filePath = "C:\Program Files\PostgreSQL\16\data\pg_hba.conf"
+# Define the text to find and the replacement text
+$oldText = "scram-sha-256"
+$newText = "trust"
+# Read file content
+$content = Get-Content -Path $filePath -Raw
+# Replace text
+$updatedContent = $content -replace [regex]::Escape($oldText), $newText
+# Write updated content back to file
+Set-Content -Path $filePath -Value $updatedContent -Encoding UTF8
+ & "C:\Program Files\PostgreSQL\16\bin\psql.exe" -U postgres -c "ALTER USER postgres WITH PASSWORD 'postgres123';"
+$newText = "scram-sha-256"
+$oldText = "trust"
+# Read file content
+$content = Get-Content -Path $filePath -Raw
+# Replace text
+$updatedContent = $content -replace [regex]::Escape($oldText), $newText
+# Write updated content back to file
+Set-Content -Path $filePath -Value $updatedContent -Encoding UTF8
